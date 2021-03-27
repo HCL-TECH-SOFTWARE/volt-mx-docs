a4f425fce6d703c3e7421b2e5f5b048b40785edd
sunilNagaraja <sunilnk@hcl.com>
2021-03-27 18:46:29 +0530

    rightNavigation fix Removed escape sequence

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -59,7 +59,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'http://localhost:4000/volt-mx-docs/assets/js/search-data.json', true);
+  request.open('GET', `https://opensource.hcltechsw.com/volt-mx-docs/assets/js/search-data.json`, true);
   
 
   request.onload = function(){
@@ -69,7 +69,7 @@ function initSearch() {
 
 
         var request1 = new XMLHttpRequest();
-        request1.open('GET', 'http://localhost:4000/volt-mx-docs/assets/js/search-index.json', true);
+        request1.open('GET', 'https://opensource.hcltechsw.com/volt-mx-docs/assets/js/search-index.json', true);
 
         request1.onload = function(){
           if (request1.status >= 200 && request1.status < 400) {
