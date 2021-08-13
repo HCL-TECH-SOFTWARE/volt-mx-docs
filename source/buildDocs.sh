958d832fedf9b6161744377618b49237ee71b6b3
Michael Stewart <michael.stewart@hcl.com>
2021-08-13 10:53:35 -0400

    More EMM changes

--- source/buildDocs.sh
+++ source/buildDocs.sh
@@ -8,15 +8,15 @@ bundle exec jekyll build $1
 cd _site/assets/js
 
 #Update the host url 
-#sed -i 's/VoltMxDocOpnSrc/pages\/phoenix-temenos\/VoltMxDocOpnSrc/g' search-data.json
-sed -i 's/`\/VoltMxDocOpnSrc/`\/pages\/phoenix-temenos\/VoltMxDocOpnSrc/g' just-the-docs.js
+sed -i 's/hcl-tech-software\.github\.io/opensource\.hcltechsw\.com/g' search-data.json
+sed -i 's/hcl-tech-software\.github\.io/opensource\.hcltechsw\.com/g' just-the-docs.js
 
 #Generate search index file
 cp search-data.json ../../../docRebrandScripts/searchIdxGeneration/
 cd ../../../docRebrandScripts/searchIdxGeneration/
 
 node createIndex.js
-#read -p "Resume"
+
 #move updated index files to _site folders 
 echo $pwrd
 cp search* $pwrd/_site/assets/js/
